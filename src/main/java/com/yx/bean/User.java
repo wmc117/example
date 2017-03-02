819a32a335e790b08724bcdbd6b020458dbc39fa
wmc117 <957146590@qq.com>
2017-03-02 22:46:14 +0800

    first commit

---
+++ src/main/java/com/yx/bean/User.java
@@ -0,0 +1,9 @@
+package com.yx.bean;
+
+/**
+ * User:WMC
+ * Date:2017/3/2.
+ */
+public class User {
+
+}
---
+++ src/main/java/com/yx/test/TestJubit.java
@@ -0,0 +1,15 @@
+package com.yx.test;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+/**
+ * Created by wmc on 2017/3/2.
+ */
+public class TestJubit {
+
+    @Test
+    public void print() {
+        StringUtils.substring("helleo", 4);
+    }
+}
